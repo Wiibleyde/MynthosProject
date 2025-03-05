89771954da0c061b23ece3ac04af5bf52d42383c
Wiibleyde <nathan@bonnell.fr>
2025-03-05 14:52:24 +0100

    Update Mynthos Paradise item to include default drink properties and remove logging from getUseAnimation method

--- src/main/java/net/wiibleyde/themynthos/item/ModItems.java
+++ src/main/java/net/wiibleyde/themynthos/item/ModItems.java
@@ -4,6 +4,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.component.Consumables;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -20,7 +21,7 @@ public class ModItems {
         () -> new Item(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse(TheMynthos.MOD_ID + ":mynthos_cheese")))));
 
     public static final RegistryObject<Item> MYNTHOS_PARADISE = ITEMS.register("mynthos_paradise",
-        () -> new DrinkItems(new Item.Properties().food(ModFoodProperties.MYNTHOS_PARADISE).setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse(TheMynthos.MOD_ID + ":mynthos_paradise")))));
+        () -> new DrinkItems(new Item.Properties().food(ModFoodProperties.MYNTHOS_PARADISE, Consumables.DEFAULT_DRINK).setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse(TheMynthos.MOD_ID + ":mynthos_paradise")))));
 
 
     public static void register(IEventBus eventBus) {
--- src/main/java/net/wiibleyde/themynthos/item/custom/DrinkItems.java
+++ src/main/java/net/wiibleyde/themynthos/item/custom/DrinkItems.java
@@ -1,22 +1,17 @@
 package net.wiibleyde.themynthos.item.custom;
 
-import com.mojang.logging.LogUtils;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUseAnimation;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
 
 public class DrinkItems extends Item {
-    private static final Logger LOGGER = LogUtils.getLogger();
-
     public DrinkItems(Properties properties) {
         super(properties);
     }
 
     @Override
     public @NotNull ItemUseAnimation getUseAnimation(@NotNull ItemStack pStack) {
-        LOGGER.info("Using drink item");
         return ItemUseAnimation.DRINK;
     }
 }
